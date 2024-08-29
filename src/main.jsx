5a4eab4e612b393f40d95cb4d0f7c278166afbb0
Kavinda Dewmith <kavindadewmith@gmail.com>
2024-08-29 16:19:23 +0530

    made changes to add to vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './input.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import './input.css';
 
-import Home from './Home.jsx'
-import Productpage from './Pages/Productpage.jsx'
-import Singleproductpage from './Pages/Singleproductpage.jsx'
-import Categorypage from './Pages/Categorypage.jsx'
-import Aboutuspage from './Pages/Aboutuspage.jsx'
-import Contactpage from './Pages/Contactpage.jsx'
+import Home from './Home.jsx';
+import Productpage from './Pages/Productpage.jsx';
+import Singleproductpage from './Pages/Singleproductpage.jsx';
+import Categorypage from './Pages/Categorypage.jsx';
+import Aboutuspage from './Pages/Aboutuspage.jsx';
+import Contactpage from './Pages/Contactpage.jsx';
 import AddTyre from './Pages/Admin/AddTyre.jsx';
 import DeleteTyre from './Pages/Admin/DeleteTyre.jsx';
 import EditTyre from './Pages/Admin/EditTyre.jsx';
-
-import { createContext, useContext, useState } from 'react';
 import EditSingleProduct from './Pages/Admin/EditSingleProduct.jsx';
 
+import { createContext, useContext, useState } from 'react';
 
 export const MyContext = createContext();
 const SearchFilterContext = createContext();
@@ -38,54 +34,27 @@ export const useSearchFilter = () => {
   return useContext(SearchFilterContext);
 };
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/products",
-    element: <Productpage />,
-  },
-  {
-    path: "/categories",
-    element: <Categorypage />,
-  },
-  {
-    path: "/about",
-    element: <Aboutuspage />,
-  },
-  {
-    path: "/contact",
-    element: <Contactpage />,
-  },
-  {
-    path: "/singleproduct/:id",
-    element: <Singleproductpage />,
-  },
-  {
-    path: "/admin",
-    element: <AddTyre />,
-  },
-  {
-    path: "/deleteTyre",
-    element: <DeleteTyre />,
-  },
-  {
-    path: "/editTyre",
-    element: <EditTyre />,
-  },
-  {
-    path: "/editSingleProduct/:id",
-    element: <EditSingleProduct />,
-  },
-]);
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Productpage />} />
+      <Route path="/categories" element={<Categorypage />} />
+      <Route path="/about" element={<Aboutuspage />} />
+      <Route path="/contact" element={<Contactpage />} />
+      <Route path="/singleproduct/:id" element={<Singleproductpage />} />
+      <Route path="/admin" element={<AddTyre />} />
+      <Route path="/deleteTyre" element={<DeleteTyre />} />
+      <Route path="/editTyre" element={<EditTyre />} />
+      <Route path="/editSingleProduct/:id" element={<EditSingleProduct />} />
+    </Routes>
+  </Router>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AppProvider>
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </AppProvider>
-)
-
+);
