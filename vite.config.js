163cd22f9164743e93ff8b01222b38b3804ea0a9
kavindadimuthu <kavindadimuthu260@gmail.com>
2025-02-01 23:59:48 +0530

    add vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr';
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  // base: '/Omilga-Tyreshop-Frontend/', // Ensure this matches your repository name
   plugins: [
     react(),
-    svgr()
+    tailwindcss(),
   ],
 })
-
-
-
-
-
 
